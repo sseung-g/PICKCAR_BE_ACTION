11233e23853497afae489a2ec109484a88f97c4f
seomoon <sunghoon1216@naver.com>
2025-05-21 14:05:49 +0900

    init: 프로젝트 초기 설정 스프링 이니셜라이저, 깃 이그노어추가

---
+++ src/main/java/com/pickcar/PickcarApplication.java
@@ -0,0 +1,13 @@
+package com.pickcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickcarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickcarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pickcar/PickcarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pickcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickcarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
