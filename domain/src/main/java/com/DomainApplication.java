9bba3f9380139825b97ce0c714b275c1c6289b6d
seomoon <sunghoon1216@naver.com>
2025-05-23 12:20:56 +0900

    chore: domain 모듈 실행 환경 구성
    secret yml 내용 작성 및 domain <- database 모듈 실행구성 추가 및
    entityscan 적용

--- domain/src/main/java/com/DomainApplication.java
+++ domain/src/main/java/com/DomainApplication.java
@@ -2,7 +2,12 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+//FIXME: Application 실행 위치
+@EntityScan(basePackages = {
+        "**.domain"         //FIXME: 와일드 카드 경로 부적절 가능성 있음
+})
 @SpringBootApplication
 public class DomainApplication {
     public static void main(String[] args) {
