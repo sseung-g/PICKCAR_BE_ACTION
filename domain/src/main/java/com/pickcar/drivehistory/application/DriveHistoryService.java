4bcf4ff673fed12e8078d3e3ff542b5308f42ab8
seomoon <sunghoon1216@naver.com>
2025-05-25 22:04:54 +0900

    feat: 운행기록 시작, 종료 일시 관련 유효성 검사 추가
    - 운행 시작, 종료 일시는 현재 시각보다 빠를 수 없음
    - 운행 종료 일시는 시작 시간보다 빠를 수 없음

--- domain/src/main/java/com/pickcar/drivehistory/application/DriveHistoryService.java
+++ domain/src/main/java/com/pickcar/drivehistory/application/DriveHistoryService.java
@@ -3,6 +3,7 @@ package com.pickcar.drivehistory.application;
 import com.pickcar.drivehistory.domain.DriveHistory;
 import com.pickcar.drivehistory.infrastructure.DriveHistoryRepository;
 import com.pickcar.drivehistory.presentation.dto.request.DriveHistoryCreateRequest;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +18,7 @@ public class DriveHistoryService {
     @Transactional
     public void create(DriveHistoryCreateRequest request) {
         //TODO: 유효성 검사, request 계산 시기 고려 필요
+        checkCondition(request);
         DriveHistory history = DriveHistory.builder()
                 .reservationId(request.reservationId())
                 .drivingStartedAt(request.drivingStartedAt())
@@ -31,5 +33,20 @@ public class DriveHistoryService {
     public DriveHistory getById(Long id) {
         return driveHistoryRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("[ERROR] DriveHistory Not Found By Id " + id));
+    }
+
+    //FIXME: 메서드 분리 및 네이밍 수정 필요, 구성 순서도 중요
+    public void checkCondition(DriveHistoryCreateRequest request) {
+        if (request.drivingStartedAt().isAfter(LocalDateTime.now())) {
+            throw new IllegalArgumentException("[ERROR] 운행 시작 일시는 현재 시각보다 빠를 수 없습니다.");
+        }
+
+        if (request.drivingEndedAt().isAfter(LocalDateTime.now())) {
+            throw new IllegalArgumentException("[ERROR] 운행 종료 일시는 현재 시각보다 빠를 수 없습니다.");
+        }
+
+        if (request.drivingEndedAt().isBefore(request.drivingStartedAt())) {
+            throw new IllegalArgumentException("[ERROR] 운행 종료 일시는 운행 시작 일시보다 빠를 수 없습니다.");
+        }
     }
 }
